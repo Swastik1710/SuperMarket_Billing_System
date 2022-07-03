0ea30788cebe0884f1f56ca9db3d736bd30cd3fb
Swastik1710 <swastikrajsingh7777@gmail.com>
2022-07-03 10:57:04 +0530

    Indentation Changes

--- main.cpp
+++ main.cpp
@@ -12,9 +12,9 @@ public:
     char price[20];
     char dis[20];
     char pname[20];
-} s[100],bill[100], t;
+} s[100], bill[100], t;
 char buffer[50], temp[50];
-int i, count = 0, key,c;
+int i, count = 0, key, c;
 fstream fp1, fp2;
 int jump[20];
 
@@ -65,17 +65,17 @@ m:
     cout << "\n\t\t\t\t\t|  2. Buyer          |";
     cout << "\n\t\t\t\t\t|                    |";
     cout << "\n\t\t\t\t\t|  3. Exit           |";
-    cout << "\n\t\t\t\t\t|                    |";
-    cout << "\n\t\t\t\t Please select:";
+    cout << "\n";
+    cout << "\n\t\t\t\tPlease select: ";
     cin >> choice;
 
     switch (choice)
     {
     case 1:
-        cout << "\t\t\t Please Login \n";
-        cout << "\t\t\t Enter Email  \n";
+        cout << "\t\t\t\tPlease Login \n";
+        cout << "\t\t\t\tEnter Email: ";
         cin >> email;
-        cout << "\t\t\t Enter Password \n";
+        cout << "\t\t\t\tEnter Password: ";
         cin >> password;
         if (email == "a" && password == "a")
         {
@@ -83,15 +83,16 @@ m:
         }
         else
         {
-            cout << "\t\t\t Invalid Email or Password \n";
+            cout << "\t\t\t\tInvalid Email or Password \n";
         }
         break;
     case 2:
         buy();
     case 3:
+        cout << "\n\t\t\t\tHave a nice day!!!";
         exit(0);
     default:
-        cout << "Please select from the given options";
+        cout << "\t\t\t\tPlease select from the given options";
     }
     goto m;
 }
@@ -100,13 +101,18 @@ void administrator()
 {
 m:
     int choice;
-    cout << "\n\n\n\t\t\t Administrator Menu";
-    cout << "\n\t\t\t|_____1. Add Product________|";
-    cout << "\n\t\t\t|_____2. Search Product_____|";
-    cout << "\n\t\t\t|_____3. Edit Product Details_______|";
-    cout << "\n\t\t\t|_____4. Remove Product_____|";
-    cout << "\n\t\t\t|_____5. Back to Main Menu_______|";
-    cout << "\n\n\t Please enter your choice ";
+    cout << "\n\t\t\t\t____________________________________";
+    cout << "\n\t\t\t\t                                    ";
+    cout << "\n\t\t\t\t         Administrator Menu         ";
+    cout << "\n\t\t\t\t                                    ";
+    cout << "\n\t\t\t\t____________________________________";
+    cout << "\n\t\t\t\t                                    ";
+    cout << "\n\t\t\t\t    |  1. Add Product           |";
+    cout << "\n\t\t\t\t    |  2. Search Product        |";
+    cout << "\n\t\t\t\t    |  3. Edit Product Details  |";
+    cout << "\n\t\t\t\t    |  4. Remove Product        |";
+    cout << "\n\t\t\t\t    |  5. Back to Main Menu     |";
+    cout << "\n\n\t\t\t\tPlease enter your choice: ";
     cin >> choice;
 
     switch (choice)
@@ -130,7 +136,7 @@ m:
         menu();
         break;
     default:
-        cout << "Please select from the given options";
+        cout << "\t\t\t\tPlease select from the given options";
     }
     goto m;
 }
@@ -162,42 +168,40 @@ void unpack()
     {
         fp1.getline(buffer, 100);
         sscanf(buffer, "%[^|]|%[^|]|%[^|]|%[^|]|", s[i].pcode, s[i].pname, s[i].price, s[i].dis);
-
-       
     }
     fp1.close();
 }
 
 void unpackOneRecord(int k)
-{   
-    fp1.open("database.txt",ios::in);
-    fp1.seekg(k*49,ios::beg);
-    fp1.getline(buffer,100);
+{
+    fp1.open("database.txt", ios::in);
+    fp1.seekg(k * 49, ios::beg);
+    fp1.getline(buffer, 100);
     sscanf(buffer, "%[^|]|%[^|]|%[^|]|%[^|]|", t.pcode, t.pname, t.price, t.dis);
     c++;
     fp1.close();
 }
 
 void write()
-{   
+{
     char tempcode[20];
-    cout << "\n\n\t\t\t Add new Product";
-    cout << "\n\t\t\t Enter the Product Code ";
+    cout << "\n\n\t\t\t\tAdd new Product";
+    cout << "\n\n\t\t\t\tEnter the Product Code: ";
     cin >> tempcode;
     int code = atoi(tempcode);
     key = code % 20;
-    while(strcmp(s[key].pcode,"xx"))
+    while (strcmp(s[key].pcode, "xx"))
     {
         key++;
         jump[key]++;
     }
-    
+
     strcpy(s[key].pcode, tempcode);
-    cout << "\n\t\t\t Enter the Product Name ";
+    cout << "\n\t\t\t\tEnter the Product Name: ";
     cin >> s[key].pname;
-    cout << "\n\t\t\t Enter the Product Price ";
+    cout << "\n\t\t\t\tEnter the Product Price: ";
     cin >> s[key].price;
-    cout << "\n\t\t\t Enter the Product Discount ";
+    cout << "\n\t\t\t\tEnter the Product Discount: ";
     cin >> s[key].dis;
     fp1.close();
     remove("database.txt");
@@ -211,100 +215,101 @@ void search()
 {
     c = 0;
     char tempcode[20];
-    cout << "\nEnter the Product Code to search for : ";
+    cout << "\n\t\t\t\tEnter the Product Code to search for: ";
     cin >> tempcode;
     int code = atoi(tempcode);
     key = code % 20;
-    unpackOneRecord(key+jump[key]);
-        
-    if(strcmp(t.pcode,tempcode)==0)
+    unpackOneRecord(key + jump[key]);
+
+    if (strcmp(t.pcode, tempcode) == 0)
     {
-            cout << "Item found!\n\n";
-            cout << "Product Code : " << t.pcode << endl;
-            cout << "Prduct Name : " << t.pname << endl;
-            cout << "Prduct Price : " << t.price << endl;
-            cout << "Discount on the Product : " << t.dis << endl;
-            
-            return;
+        cout << "\n\t\t\t\tItem found!";
+        cout << "\n\n\t\t\t\tProduct Code : " << t.pcode;
+        cout << "\n\t\t\t\tPrduct Name : " << t.pname;
+        cout << "\n\t\t\t\tPrduct Price : " << t.price;
+        cout << "\n\t\t\t\tDiscount on the Product : " << t.dis;
+
+        return;
     }
-    cout << "Item not found\n";
+    cout << "\n\t\t\t\tItem not found\n";
 }
 
 void modify()
 {
     int tempKey;
-    
+
     char tempcode[20];
-    cout << "\n\n\t\t\t Modify Product Details";
-    cout << "\n\t\t\t Enter the Product Code ";
+    cout << "\n\n\t\t\t\tModify Product Details";
+    cout << "\n\t\t\t\tEnter the Product Code ";
     cin >> tempcode;
     int code = atoi(tempcode);
     key = code % 20;
-    if(strcmp(s[key+jump[key]].pcode,tempcode)!=0)
+    if (strcmp(s[key + jump[key]].pcode, tempcode) != 0)
     {
-        cout << "\nItem not found\n";
+        cout << "\n\t\t\t\tItem not found\n";
         return;
     }
-    cout << "Enter the Modified Product details";
-    cout << "\n Enter the Product Name ";
-    cin >> s[key+jump[key]].pname;
-    cout << "\n Enter the Product Price ";
-    cin >> s[key+jump[key]].price;
-    cout << "\n Enter the Product Discount ";
-    cin >> s[key+jump[key]].dis;
+    cout << "\n\t\t\t\tEnter the Modified Product details";
+    cout << "\n\t\t\t\tEnter the Product Name ";
+    cin >> s[key + jump[key]].pname;
+    cout << "\n\t\t\t\tEnter the Product Price ";
+    cin >> s[key + jump[key]].price;
+    cout << "\n\t\t\t\tEnter the Product Discount ";
+    cin >> s[key + jump[key]].dis;
     fp1.close();
     remove("database.txt");
     fp1.open("database.txt", ios::out);
     fp1.close();
     for (int j = 0; j < 20; j++)
         pack(s[j]);
-    }
+}
 
 void remove()
 {
     int tempKey;
-    
     char tempcode[20];
-    cout << "\n\n\t\t\t Delete Product";
-    cout << "\n\t\t\t Enter the Product Code ";
+    cout << "\n\n\t\t\t\tDelete Product";
+    cout << "\n\t\t\t\tEnter the Product Code ";
     cin >> tempcode;
     int code = atoi(tempcode);
     key = code % 20;
-    if(strcmp(s[key+jump[key]].pcode,tempcode)!=0)
+    if (strcmp(s[key + jump[key]].pcode, tempcode) != 0)
     {
-        cout << "\nItem not found\n";
+        cout << "\n\t\t\t\tItem not found\n";
         return;
     }
-    cout << "Item found!\n\n";
-    cout << "Product Code : " << s[key + jump[key]].pcode << endl;
-    cout << "Prduct Name : " << s[key + jump[key]].pname << endl;
-    cout << "Prduct Price : " << s[key + jump[key]].price << endl;
-    cout << "Discount on the Product : " << s[key + jump[key]].dis << endl;
+    cout << "\n\t\t\t\tItem found!";
+    cout << "\n\n\t\t\t\tProduct Code : " << s[key + jump[key]].pcode;
+    cout << "\n\t\t\t\tPrduct Name : " << s[key + jump[key]].pname;
+    cout << "\n\t\t\t\tPrduct Price : " << s[key + jump[key]].price;
+    cout << "\n\t\t\t\tDiscount on the Product : " << s[key + jump[key]].dis;
 
     strcpy(s[key + jump[key]].pcode, "xx");
     strcpy(s[key + jump[key]].pname, "xx");
     strcpy(s[key + jump[key]].price, "xx");
     strcpy(s[key + jump[key]].dis, "xx");
-    cout << "ITEM DELETED!";
+    cout << "\n\n\t\t\t\tITEM DELETED!";
     fp1.close();
     remove("database.txt");
     fp1.open("database.txt", ios::out);
     fp1.close();
     for (int j = 0; j < 20; j++)
         pack(s[j]);
-
-
 }
-
 
 void buy()
 {
-    m:
+m:
     int choice;
-    cout << "\n\t\t\t\t\t Menu";
-    cout << "\n\t\t\t|_________1. Buy the Product_________|";
-    cout << "\n\t\t\t|_________2. Back to Main Menu_______|";
-    cout << "\n\n\t Please enter your choice ";
+    cout << "\n\t\t\t\t_____________________________";
+    cout << "\n\t\t\t\t                             ";
+    cout << "\n\t\t\t\t            Menu             ";
+    cout << "\n\t\t\t\t                             ";
+    cout << "\n\t\t\t\t_____________________________";
+    cout << "\n\t\t\t\t                             ";
+    cout << "\n\t\t\t\t  | 1. Buy the Product    |  ";
+    cout << "\n\t\t\t\t  | 2. Back to Main Menu  |  ";
+    cout << "\n\n\t\t\t\tPlease enter your choice ";
     cin >> choice;
 
     switch (choice)
@@ -316,77 +321,72 @@ void buy()
         menu();
         break;
     default:
-        cout << "Please select from the given options";
+        cout << "\t\t\t\tPlease select from the given options";
     }
     goto m;
 }
-
 
 void receipt()
 {
     char tempcode[20];
     int quantity[20];
     int totalProd = 0;
-    float amount, totalAmount,discount;
+    float amount, totalAmount, discount;
     char ch;
-    
-   
-        fp1.close();
-        cout << "\n______________________________________\n";
-        cout << "\n        Please place the order        \n";
-        cout << "\n______________________________________\n";
+    fp1.close();
+    cout << "\n\t\t\t\t________________________________";
+    cout << "\n\t\t\t\t                                ";
+    cout << "\n\t\t\t\t     Please place the order     ";
+    cout << "\n\t\t\t\t                                ";
+    cout << "\n\t\t\t\t________________________________";
+    cout << "\n\t\t\t\t                                ";
+    do
+    {
+        cout << "\n\t\t\t\tEnter the Product Code : ";
+        cin >> tempcode;
+        int code = atoi(tempcode);
+        key = code % 20;
+        unpackOneRecord(key + jump[key]);
 
-        do
+        if (strcmp(t.pcode, tempcode) != 0)
         {
-            cout << "\nEnter the Product Code : ";
-            cin >> tempcode;
-            int code = atoi(tempcode);
-            key = code % 20;
-            unpackOneRecord(key+jump[key]);
-
-            if(strcmp(t.pcode,tempcode)!=0)
-            {
-                cout << "\nItem not found\n";
-                
-            }
-
-            else
-            {
-                strcpy(bill[totalProd].pcode, t.pcode);
-                strcpy(bill[totalProd].pname, t.pname);
-                strcpy(bill[totalProd].price, t.price);
-                strcpy(bill[totalProd].dis, t.dis);
-                
-                cout << "\n\t\t\t Enter the Quantity ";
-                cin >> quantity[totalProd];
-
-                totalProd++;
-            }
-
-            cout << "\n\t\t\t Do you want to order more (y/n) ";
-            cin >> ch;
-        } while (ch == 'y');
-
-        cout << "\n\n\t\t\t__________________RECEIPT____________________\n";
-        cout << "\nProductNo\tProductName\tQuantity\tPrice\tAmount\t Amount after Discount\n";
-
-        for (int i = 0; i < totalProd; i++)
-        {
-            amount = atoi(bill[i].price) * quantity[i];
-            discount = (atoi(bill[i].dis) * amount) / 100.0;
-
-            totalAmount = amount - discount ;
-            cout << bill[i].dis << "\n";
-            cout << bill[i].pcode << "\t" << bill[i].pname << "\t" << quantity[i] << "\t" << bill[i].price << "\t" << amount << "\t" << totalAmount << "\n";
+            cout << "\n\t\t\t\tItem not found\n";
         }
-    
-}      
-            
-        
+
+        else
+        {
+            strcpy(bill[totalProd].pcode, t.pcode);
+            strcpy(bill[totalProd].pname, t.pname);
+            strcpy(bill[totalProd].price, t.price);
+            strcpy(bill[totalProd].dis, t.dis);
+
+            cout << "\n\t\t\t\tEnter the Quantity ";
+            cin >> quantity[totalProd];
+
+            totalProd++;
+        }
+
+        cout << "\n\t\t\t\tDo you want to order more (y/n) ";
+        cin >> ch;
+    } while (ch == 'y');
+
+    cout << "\n\n\t\t\t____________________RECEIPT____________________\n";
+    cout << "\n\tProductNo\tProductName\tQuantity\tPrice\tAmount\tAmount after Discount\n";
+
+    for (int i = 0; i < totalProd; i++)
+    {
+        amount = atoi(bill[i].price) * quantity[i];
+        discount = (atoi(bill[i].dis) * amount) / 100.0;
+
+        totalAmount = amount - discount;
+        cout << "\t" << bill[i].pcode << "\t\t" << bill[i].pname << "\t\t" << quantity[i] << "\t\t" << bill[i].price << "\t" << amount << "\t\t" << totalAmount << "\n";
+    }
+}
+
 int main()
 {
-    //init();
-    for (int i = 0; i < 20;i++)
+    // init();
+    for (int i = 0; i < 20; i++)
         jump[i] = 0;
     menu();
     return 0;
